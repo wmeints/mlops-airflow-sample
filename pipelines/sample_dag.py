328e617ba1f79167d5ce381ffe7653294ab5da70
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-09-08 13:56:07 +0200

    feat: Add basic DAG

---
+++ pipelines/sample_dag.py
@@ -0,0 +1,29 @@
+from re import M
+from airflow.decorators import dag, task
+import pendulum
+
+
+@dag(
+    schedule_interval=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz='UTC'),
+    catchup=False,
+    tags=['sample']
+)
+def sample_etl():
+    @task()
+    def extract():
+        pass
+
+    @task()
+    def transform():
+        pass
+
+    @task()
+    def load():
+        pass
+
+    extract()
+    transform()
+    load()
+
+sample_dag = sample_etl()
