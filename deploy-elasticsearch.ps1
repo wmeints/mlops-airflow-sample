f0baa81d97b2df2e5fcd6f411be3e38f09638287
Willem Meints <1550763+wmeints@users.noreply.github.com>
2022-09-09 21:10:21 +0200

    feat: Add filebeat and logstash

--- deploy-elasticsearch.ps1
+++ deploy-elasticsearch.ps1
@@ -3,3 +3,5 @@ helm repo update
 
 helm upgrade --install elasticsearch elastic/elasticsearch -f ./deploy/elasticsearch/values-override.yml --namespace logging --create-namespace
 helm upgrade --install kibana elastic/kibana -f ./deploy/kibana/values-override.yml --namespace logging --create-namespace
+helm upgrade --install filebeat elastic/filebeat -f ./deploy/filebeat/values-override.yml --namespace logging --create-namespace
+helm upgrade --install logstash elastic/logstash -f ./deploy/logstash/values-override.yml --namespace logging --create-namespace
