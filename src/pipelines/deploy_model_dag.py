de591c6812c5ea7ec292fd939b977763baaaf471
Emiel Stoelinga <emielstoelinga@gmail.com>
2022-10-31 14:00:53 +0100

    deploy model DAG

---
+++ src/pipelines/deploy_model_dag.py
@@ -0,0 +1,18 @@
+from airflow.decorators import dag
+import pendulum
+from tasks.deploy import deploy
+
+
+@dag(
+    schedule_interval=None,
+    start_date=pendulum.datetime(2021,1,1,tz='UTC'),
+    catchup=False,
+    tags=['wachttijden']
+)
+def deploy_model():
+    deploy({
+        'artifact_url': '<artifact-uri>'
+    })
+
+
+deploy_model_dag = deploy_model()
---
+++ src/pipelines/tasks/deploy.py
@@ -0,0 +1,41 @@
+from airflow.decorators import task
+
+
+@task.virtualenv(
+    use_dill=True,
+    system_site_packages=True,
+    requirements=[
+        'kubernetes==25.3.0',
+        'kserve==0.9.0'
+    ]
+)
+def deploy(input_data):
+    from kubernetes import client
+    from kserve import KServeClient
+    from kserve import constants
+    from kserve import V1beta1InferenceService
+    from kserve import V1beta1InferenceServiceSpec
+    from kserve import V1beta1PredictorSpec
+    from kserve import V1beta1ModelSpec
+    from kserve import V1beta1ModelFormat
+
+    namespace = 'knative-serving'
+
+    modelspec = V1beta1ModelSpec(
+        storage_uri=input_data['artifact_url'],
+        model_format=V1beta1ModelFormat(name='mlflow'))
+
+    api_version = constants.KSERVE_V1BETA1
+
+    isvc = V1beta1InferenceService(api_version=api_version,
+                                kind=constants.KSERVE_KIND,
+                                metadata=client.V1ObjectMeta(
+                                    name='mlflow-wachttijden-tree', namespace=namespace),
+                                spec=V1beta1InferenceServiceSpec(
+                                    predictor=V1beta1PredictorSpec(
+                                        model=modelspec))
+                                )
+
+    KServe = KServeClient()
+    KServe.create(isvc)
+
