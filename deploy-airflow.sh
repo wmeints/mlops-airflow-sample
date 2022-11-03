14e7a4905da8f7b35efdfb4ed340c9ae68fc425e
Emiel Stoelinga <emielstoelinga@gmail.com>
2022-11-03 11:13:32 +0100

    add clusterrolebinding airflow-worker sa

--- deploy-airflow.sh
+++ deploy-airflow.sh
@@ -8,3 +8,6 @@ helm upgrade --install airflow apache-airflow/airflow \
     -f deploy/airflow/values-override.yml \
     -f deploy/airflow/values-secrets.yml \
     --create-namespace
+
+# create clusterrolebinding such that the Airflow worker serviceaccount can deploy KServe InferenceServices
+kubectl create clusterrolebinding airflow-worker-kserve-manager-rolebinding --clusterrole=kserve-manager-role --serviceaccount=airflow:airflow-worker
