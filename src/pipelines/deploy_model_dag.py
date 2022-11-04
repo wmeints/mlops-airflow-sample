779849aad7c71a2c516372d895264f9965922444
Emiel Stoelinga <emielstoelinga@gmail.com>
2022-11-04 16:15:29 +0100

    remove var default artifact_url

--- src/pipelines/deploy_model_dag.py
+++ src/pipelines/deploy_model_dag.py
@@ -4,8 +4,6 @@ from tasks.deploy_single_model import deploy_single_model
 from airflow.models import Variable
 from utils.utils import check_default_artifact_url
 
-default_artifact_url = '<artifact-url>'
-
 
 @dag(
     schedule_interval=None,
@@ -14,10 +12,10 @@ default_artifact_url = '<artifact-url>'
     tags=['wachttijden']
 )
 def deploy_model(
-        deploy_model_artifact_url: str = Variable.get('deploy_model_artifact_url', default_var=default_artifact_url),
+        deploy_model_artifact_url: str = Variable.get('deploy_model_artifact_url', default_var='<artifact-url>'),
         deploy_model_model_name: str = Variable.get('deploy_model_model_name', default_var='mlflow-wachttijden-tree')):
 
-    check_default_artifact_url(default_artifact_url, deploy_model_artifact_url)
+    check_default_artifact_url('<artifact-url>', deploy_model_artifact_url)
 
     deploy_single_model({
         'artifact_url': deploy_model_artifact_url,
